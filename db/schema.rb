70c55a7b47af9683a8972328ce0c9cecbc0ea1cb
James Dawson <jamesdawson707@gmail.com>
2017-09-12 03:02:55 +0000

    Added plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905044859) do
+ActiveRecord::Schema.define(version: 20170906033659) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
